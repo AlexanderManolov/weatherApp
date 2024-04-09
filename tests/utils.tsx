0c30d56cd7b4d3fb170807df649e0e796058d202
alexander.manolov <alexander.manolov@ableto.com>
2024-04-10 00:07:02 +0300

    Add more test setup.

--- tests/utils.tsx
+++ tests/utils.tsx
@@ -2,7 +2,9 @@ import { ReactNode } from 'react'
 import GlobalStyle from '../src/components/GlobalStyle'
 import { GlobalContextProvider } from '../src/contexts/GlobalContext/GlobalContext'
 
-type ElementProps = { children: ReactNode }
+type ElementProps = {
+  children?: ReactNode
+}
 
 export const ContextProvider = ({ children }: ElementProps) => {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+import EnvironmentPlugin from 'vite-plugin-environment'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/',
+  root: '.',
+  plugins: [react(), viteTsconfigPaths(), EnvironmentPlugin('all')],
+  server: {
+    // this ensures that the browser opens upon server start
+    open: true,
+    port: 3000,
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.ts'],
+    globals: true,
+    coverage: {
+      provider: 'v8'
+    }
+  }
 })
